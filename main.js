c3713eb0078e4783cd5f3f2b51986b7866e3a78a
Gavinthecat <daniel.bader@bws.ac.at>
2024-11-05 10:52:59 +0100

    Test

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const tableDetail = document.querySelectorAll("td");
 
+tableDetail.forEach(td => {
+    const computedStyle = window.getComputedStyle(td);
+    const height = computedStyle.height;
+
+    td.style.width = height;
+    
+});
