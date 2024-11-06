ced4c407f0147e04548bf32aa9f94d89f2e8e8ed
Gavinthecat <daniel.bader@bws.ac.at>
2024-11-06 15:39:51 +0100

    small update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     const computedStyle = window.getComputedStyle(button);
-    const height = parseFloat(computedStyle.height) * 1.75;
+    const height = parseFloat(computedStyle.height) * 1.5;
 
     button.style.width = `${height}px`;
 });
